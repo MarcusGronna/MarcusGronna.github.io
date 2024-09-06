8366f5be947b4766a0f8bc2e288b48acc7251ebe
Marcus <mgronna@gmail.com>
2024-09-06 13:54:52 +0200

    Same as last

--- scripts/script.js
+++ scripts/script.js
@@ -11,12 +11,32 @@ catAttackButton.addEventListener("click", function () {
 });
 
 $(".click-text-about").click(function () {
-   $(".toggle-text-about").toggle();
+   if ($(".toggle-text-about").css("display") === "none") {
+      $(".toggle-text-about").fadeIn(300).css("display", "block");
+   } else {
+      $(".toggle-text-about").fadeOut(300);
+   }
 });
 
 $(".click-text-portfolio-1").click(function () {
-   $(".toggle-text-portfolio-1").toggle();
-   $(".toggle-text-portfolio-2").hide();
+   let isMobile = window.matchMedia("(max-width: 480px)").matches; //Next to do!
+
+   if (isMobile) {
+      if ($(".toggle-text-portfolio-1.small-screen").css("display") === "none") {
+         $(".toggle-text-portfolio-1.small-screen").fadeIn(300).css("display", "block");
+         $(".toggle-text-portfolio-2").hide();
+      } else {
+         $(".toggle-text-portfolio-1").fadeOut(300);
+      }
+   } else {
+      if ($(".toggle-text-portfolio-1").css("display") === "none") {
+         $(".toggle-text-portfolio-1").fadeIn(300).css("display", "block");
+         $(".toggle-text-portfolio-2").hide();
+      } else {
+         $(".toggle-text-portfolio-1").fadeOut(300);
+      }
+   }
+   // $(".toggle-text-portfolio-1").toggle();
 });
 
 $(".click-text-portfolio-2").click(function () {
