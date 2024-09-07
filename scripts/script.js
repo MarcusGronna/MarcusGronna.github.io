f6260ae7c9f10015a1ef25ae3482c5cfad2c8e48
fuzzylokis <161626354+fuzzylokis@users.noreply.github.com>
2024-09-07 10:30:46 +0200

    New branch and big changes with jQuery

--- scripts/script.js
+++ scripts/script.js
@@ -10,13 +10,62 @@ catAttackButton.addEventListener("click", function () {
    window.open("./Projekt/cat_attack.html", "Cat Attack", "width=device-width, resizable=yes");
 });
 
+let colorTheme = "first";
+$("#invert-colors").click(function () {
+   if (colorTheme === "first") {
+      $(":root").css({
+         "--primary-color": "#F8EDE3",
+         "--secondary-color": "#798777",
+         "--primary-contrast-color": "#798777",
+         "--secondary-contrast-color": "#F8EDE3",
+         "--font-color": "#F8EDE3",
+         "--link-color": "#F8EDE3",
+         "--button-color": "#F8EDE3",
+         "--accent-color": "#596358",
+      });
+      colorTheme = "second";
+   } else {
+      $(":root").css({
+         "--primary-color": "#798777",
+         "--secondary-color": "#F8EDE3",
+         "--primary-contrast-color": "#F8EDE3",
+         "--secondary-contrast-color": "#798777",
+         "--accent-color": "#BDD2B6",
+         "--button-color": "#596358",
+         "--font-color": "#596358",
+         "--link-color": "#596358",
+      });
+      colorTheme = "first";
+   }
+});
+
 $(".click-text-about").click(function () {
-   $(".toggle-text-about").toggle();
+   if ($(".toggle-text-about").css("display") === "none") {
+      $(".toggle-text-about").fadeIn(300).css("display", "block");
+   } else {
+      $(".toggle-text-about").fadeOut(300);
+   }
 });
 
 $(".click-text-portfolio-1").click(function () {
-   $(".toggle-text-portfolio-1").toggle();
-   $(".toggle-text-portfolio-2").hide();
+   let isMobile = window.matchMedia("(max-width: 480px)").matches; //Next to do!
+
+   if (isMobile) {
+      if ($(".toggle-text-portfolio-1.small-screen").css("display") === "none") {
+         $(".toggle-text-portfolio-1.small-screen").fadeIn(300).css("display", "block");
+         $(".toggle-text-portfolio-2").hide();
+      } else {
+         $(".toggle-text-portfolio-1").fadeOut(300);
+      }
+   } else {
+      if ($(".toggle-text-portfolio-1.normal-screen").css("display") === "none") {
+         $(".toggle-text-portfolio-1.normal-screen").fadeIn(300).css("display", "block");
+         $(".toggle-text-portfolio-2").hide();
+      } else {
+         $(".toggle-text-portfolio-1.normal-screen").fadeOut(300);
+      }
+   }
+   // $(".toggle-text-portfolio-1").toggle();
 });
 
 $(".click-text-portfolio-2").click(function () {
